945383015b3fb15f6d379b3b00339ddc8c1ea612
mauriciosalass <maurisalas.s@uc.cl>
2026-03-31 23:29:54 -0300

    Quitar verificacion de verbos

--- task_validator.py
+++ task_validator.py
@@ -1,10 +1,9 @@
 import re
-from config import VERBOS_ACCION, FECHA_REGEX, HORA_REGEX
+from config import FECHA_REGEX, HORA_REGEX
 
 def es_tarea_valida(texto):
     texto = texto.lower()
-    hay_verbo = any(verbo in texto for verbo in VERBOS_ACCION)
     hay_fecha = bool(re.search(FECHA_REGEX, texto))
     hay_hora = bool(re.search(HORA_REGEX, texto))
 
-    return hay_verbo and hay_fecha and hay_hora
+    return hay_fecha and hay_hora
